849678256ce75991ea75fa1b30ede1c8f4132adf
jellie <talentforest0501@gmail.com>
2022-08-29 01:36:34 +0900

    Change css in votecreatebox

--- src/components/vote/VoteCreateBox.tsx
+++ src/components/vote/VoteCreateBox.tsx
@@ -1,11 +1,11 @@
 import { Add, CheckCircleOutline, Close } from "@mui/icons-material";
 import { useState } from "react";
-import { ko } from "date-fns/esm/locale";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import styled from "styled-components";
 import useCreateVoteBox from "hooks/useCreateVoteBox";
+import styled from "styled-components";
 import device from "theme/mediaQueries";
+import DatePicker from "react-datepicker";
+import { ko } from "date-fns/esm/locale";
+import "react-datepicker/dist/react-datepicker.css";
 
 interface PropsType {
   setModalOpen: (modalOpen: boolean) => void;
@@ -14,61 +14,66 @@ interface PropsType {
 const VoteCreateBox = ({ setModalOpen }: PropsType) => {
   const [endDate, setEndDate] = useState(new Date());
 
-  const { vote, onSubmit, onChange, onPlusClick, onDeleteClick } =
-    useCreateVoteBox(setModalOpen, endDate);
+  const {
+    vote,
+    onRegisterSubmit,
+    onTitleChange,
+    onItemPlusClick,
+    onItemDeleteClick,
+  } = useCreateVoteBox(setModalOpen, endDate);
 
   return (
-    <CreateBox onSubmit={onSubmit}>
-      <div>
-        <Vote>
-          <span>투표 제목</span>
-          <Input
-            type="text"
-            placeholder="등록하실 투표의 제목을 적어주세요."
-            value={vote.title}
-            onChange={onChange}
-            name="title"
-            required
-          />
-          <ul>
-            <span>투표 항목</span>
-            {vote.voteItem.map((item, index: number) => (
-              <li key={index}>
-                <CheckCircleOutline />
-                <Input
-                  type="text"
-                  placeholder="투표항목을 적어주세요."
-                  name={`vote_item${index}`}
-                  value={item.item}
-                  onChange={(event) => onChange(event, index)}
-                  required
-                />
-                {index > 1 && <Close onClick={() => onDeleteClick(index)} />}
-              </li>
-            ))}
-          </ul>
-          <AddVoteItem onClick={onPlusClick}>
-            <Add />
-            투표항목 추가하기
-          </AddVoteItem>
-          {vote.voteItem.length > 7 && (
-            <span>투표항목은 8개를 넘을 수 없습니다.</span>
-          )}
-        </Vote>
-        <Deadline>
-          <span>투표 종료일</span>
-          <DatePick
-            name="datepick"
-            selected={endDate}
-            onChange={(date: Date) => setEndDate(date)}
-            selectsEnd
-            endDate={endDate}
-            minDate={new Date()}
-            locale={ko}
-            dateFormat="yyyy년 MM월 dd일"
-          />
-        </Deadline>
-      </div>
+    <CreateBox onSubmit={onRegisterSubmit}>
+      <Vote>
+        <h1>투표 제목</h1>
+        <Input
+          type="text"
+          placeholder="투표의 제목을 적어주세요."
+          value={vote.title}
+          onChange={onTitleChange}
+          name="title"
+          required
+        />
+        <h2>투표 항목</h2>
+        <ul>
+          {vote.voteItem.map((item) => (
+            <li key={item.id}>
+              <CheckCircleOutline />
+              <Input
+                type="text"
+                placeholder="투표항목을 적어주세요."
+                name={`vote_item${item.id}`}
+                value={item.item}
+                onChange={(event) => onTitleChange(event, item.id)}
+                required
+              />
+              {item.id > 2 && (
+                <Close onClick={() => onItemDeleteClick(item.id)} />
+              )}
+            </li>
+          ))}
+        </ul>
+        <AddVoteItem onClick={onItemPlusClick}>
+          <Add />
+          투표항목 추가하기
+        </AddVoteItem>
+        {vote.voteItem.length > 7 && (
+          <span>투표항목은 8개를 넘을 수 없습니다.</span>
+        )}
+      </Vote>
+      <Deadline>
+        <span>투표 종료일 설정</span>
+        <DatePick
+          name="datepick"
+          selected={endDate}
+          onChange={(date: Date) => setEndDate(date)}
+          selectsEnd
+          endDate={endDate}
+          minDate={new Date()}
+          locale={ko}
+          dateFormat="yyyy년 MM월 dd일"
+        />
+      </Deadline>
       <button type="submit">투표 등록하기</button>
     </CreateBox>
   );
@@ -76,25 +81,21 @@ const VoteCreateBox = ({ setModalOpen }: PropsType) => {
 
 const CreateBox = styled.form`
   z-index: 2;
-  position: fixed;
   overflow: scroll;
+  position: fixed;
   top: 0;
   right: 0;
   left: 0;
-  min-height: 30vh;
-  max-height: 80vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  margin: 30px;
-  padding: 15px;
+  min-height: 30vh;
+  max-height: 80vh;
+  margin: 40px;
+  padding: 20px;
   border-radius: 5px;
-  box-shadow: 1px 2px 5px rgba(0, 0, 0, 0.3);
-  background-color: ${(props) => props.theme.container.purple};
-  > div {
-    width: 100%;
-  }
+  background-color: ${(props) => props.theme.container.blue};
   > button {
     cursor: pointer;
     font-size: 16px;
@@ -139,25 +140,26 @@ const Input = styled.input`
 
 const Vote = styled.div`
   margin-bottom: 10px;
-  padding: 10px;
+  padding: 15px;
   width: 100%;
   font-size: 16px;
   border-radius: 5px;
   background-color: ${(props) => props.theme.container.default};
-  > span {
+  > h1 {
     display: block;
     margin-bottom: 6px;
     font-weight: 700;
     font-size: 16px;
+    color: ${(props) => props.theme.text.accent};
+  }
+  > h2 {
+    display: block;
+    font-size: 16px;
+    margin: 20px 0 6px;
+    font-weight: 700;
+    color: ${(props) => props.theme.text.accent};
   }
   > ul {
-    margin-top: 20px;
-    span {
-      display: block;
-      font-size: 16px;
-      margin-bottom: 6px;
-      font-weight: 700;
-    }
     li {
       position: relative;
       display: flex;
@@ -168,8 +170,9 @@ const Vote = styled.div`
       }
       svg {
         margin-right: 5px;
-        width: 16px;
-        height: 16px;
+        width: 18px;
+        height: 18px;
+        fill: ${(props) => props.theme.text.accent};
         &:last-child {
           position: absolute;
           right: 0;
@@ -212,16 +215,18 @@ const Vote = styled.div`
 `;
 
 const Deadline = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   border-radius: 5px;
-  padding: 20px 10px;
+  padding: 15px;
   background-color: ${(props) => props.theme.container.default};
   span {
     display: block;
     font-size: 16px;
     font-weight: 700;
     margin-bottom: 8px;
+    color: ${(props) => props.theme.text.accent};
   }
   @media ${device.tablet} {
     span {
@@ -254,8 +259,8 @@ const AddVoteItem = styled.div`
   cursor: pointer;
   display: flex;
   align-items: center;
-  font-size: 13px;
-  margin-top: 15px;
+  font-size: 16px;
+  margin-top: 20px;
   color: ${(props) => props.theme.text.accent};
   svg {
     width: 18px;
