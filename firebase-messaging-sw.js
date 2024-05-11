03e4d1764c6ebeb82d47618dadd7d9b914122796
jellie <talentforest0501@gmail.com>
2024-05-11 12:31:58 +0900

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,6 +2,20 @@
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyA6HW3pwkb-agsEpSYWGmlYSuvFiJDxp5c',
+  authDomain: 'han-bookclub.firebaseapp.com',
+  projectId: 'han-bookclub',
+  storageBucket: 'han-bookclub.appspot.com',
+  messagingSenderId: '1033530409448',
+  appId: '1:1033530409448:web:aff6942a34c0a48c81645d',
+  measurementId: 'G-925LMFR6FK',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = firebase.messaging(firebaseApp);
+
 /* eslint-disable no-restricted-globals */
 self.addEventListener('install', function (e) {
   self.skipWaiting();
