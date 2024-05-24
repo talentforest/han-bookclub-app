7e529e80638c2182a4411b84903b4be426c4dd41
jellie <talentforest0501@gmail.com>
2024-05-24 22:52:43 +0900

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+importScripts(
+  'https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js'
+);
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA6HW3pwkb-agsEpSYWGmlYSuvFiJDxp5c',
+  authDomain: 'han-bookclub.firebaseapp.com',
+  projectId: 'han-bookclub',
+  storageBucket: 'han-bookclub.appspot.com',
+  messagingSenderId: '1033530409448',
+  appId: '1:1033530409448:web:aff6942a34c0a48c81645d',
+  measurementId: 'G-925LMFR6FK',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  );
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/han-bookclub-app/hanpage_logo.png',
+  };
+
+  // eslint-disable-next-line no-restricted-globals
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
