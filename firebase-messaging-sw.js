5d740975115076c16ca2f203bbc92f4a0aabd1bc
jellie <talentforest0501@gmail.com>
2024-05-29 11:01:29 +0900

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -7,6 +7,18 @@ importScripts(
   'https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js'
 );
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyA6HW3pwkb-agsEpSYWGmlYSuvFiJDxp5c',
+  authDomain: 'han-bookclub.firebaseapp.com',
+  projectId: 'han-bookclub',
+  storageBucket: 'han-bookclub.appspot.com',
+  messagingSenderId: '1033530409448',
+  appId: '1:1033530409448:web:aff6942a34c0a48c81645d',
+  measurementId: 'G-925LMFR6FK',
+};
+
+firebase.initializeApp(firebaseConfig);
+
 /* eslint-disable no-restricted-globals */
 self.addEventListener('install', function (e) {
   self.skipWaiting();
@@ -23,36 +35,8 @@ self.addEventListener('push', function (e) {
   const notificationTitle = resultData.title;
   const notificationOptions = {
     body: resultData.body,
+    icon: resultData.notification.icon,
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyA6HW3pwkb-agsEpSYWGmlYSuvFiJDxp5c',
-  authDomain: 'han-bookclub.firebaseapp.com',
-  projectId: 'han-bookclub',
-  storageBucket: 'han-bookclub.appspot.com',
-  messagingSenderId: '1033530409448',
-  appId: '1:1033530409448:web:aff6942a34c0a48c81645d',
-  measurementId: 'G-925LMFR6FK',
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// const messaging = firebase.messaging();
-
-// messaging.onBackgroundMessage((payload) => {
-//   console.log(
-//     '[firebase-messaging-sw.js] Received background message ',
-//     payload
-//   );
-//   const notificationTitle = payload.notification.title;
-//   const notificationOptions = {
-//     body: payload.notification.body,
-//     icon: '/han-bookclub-app/hanpage_logo.png',
-//   };
-
-//   // eslint-disable-next-line no-restricted-globals
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// });
