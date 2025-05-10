07bad48ee757892565b333ec9a1149cbdb13f8ad
jellie <talentforest0501@gmail.com>
2025-05-10 12:11:23 +0900

    design: 모임 시간 장소 css 수정

--- client/src/components/common/CustomDatePicker.tsx
+++ client/src/components/common/CustomDatePicker.tsx
@@ -27,7 +27,7 @@ export default function CustomDatePicker({
         locale={ko}
         minDate={new Date()}
         dateFormat="MM월 dd일 a hh:mm"
-        shouldCloseOnSelect={false}
+        shouldCloseOnSelect
         popperPlacement="bottom"
         className={`flex h-12 w-full items-center rounded-xl border border-gray2 !py-3 !pl-10 outline-none ${className}`}
         showTimeInput
@@ -36,14 +36,7 @@ export default function CustomDatePicker({
         timeFormat="HH:mm"
         timeIntervals={10}
         onFocus={e => e.target.blur()}
-      >
-        <button
-          type="button"
-          className="absolute -bottom-8 -left-[1px] -right-[1px] rounded-b-xl border border-t-0 border-gray2 bg-green1 py-2.5 font-SUITE text-[15px] text-darkBlue"
-        >
-          완료
-        </button>
-      </DatePicker>
+      />
     </div>
   );
 }
